2d00de54d972911b44bfd42708a9d58bd2dfe84f
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-11-29 16:18:29 +0100

    first commit

---
+++ src/components/TheFooter.jsx
@@ -0,0 +1,11 @@
+import style from "../css/modules/TheFooter.module.css";
+export default function TheFooter() {
+    return (
+        <>
+            <footer className={style.FooterStyle}>
+                <p className={style.FooterContent}>Made with <span>&hearts</span> React1</p>
+            </footer>
+        </>
+    )
+
+}
