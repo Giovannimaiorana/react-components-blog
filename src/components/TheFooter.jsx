2c3340305a1af8177989a6d92cab25c496fbc160
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-11-29 16:19:15 +0100

    Esercizio concluso + Bonus

--- src/components/TheFooter.jsx
+++ src/components/TheFooter.jsx
@@ -3,7 +3,7 @@ export default function TheFooter() {
     return (
         <>
             <footer className={style.FooterStyle}>
-                <p className={style.FooterContent}>Made with <span>&hearts</span> React1</p>
+                <p className={style.FooterContent}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis id odit illo! Est non, mollitia inventore doloribus vero illum ipsa explicabo obcaecati quibusdam, ipsam porro quam repellat perspiciatis corporis beatae!</p>
             </footer>
         </>
     )
